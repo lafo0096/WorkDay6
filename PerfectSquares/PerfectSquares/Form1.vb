9e3ef3c823071f78445e16b86e1662b307a3253c
idsc3101 <idsc3101@gmail.com>
2013-10-09 17:13:01 -0500

    These work day problems are done!

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -3,7 +3,14 @@
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
 
         lstResult.Items.Clear()
-        lstResult.Items.Add("Not implemented yet")
+
+        Dim n As Integer = 1
+
+        Do
+            Dim square As Integer = n ^ 2
+            lstResult.Items.Add(square)
+            n += 1
+        Loop Until n > 10
 
     End Sub
 End Class
--- PopulationSim/PopulationSim/Form1.vb
+++ PopulationSim/PopulationSim/Form1.vb
@@ -4,7 +4,15 @@ Public Class Form1
 
     Private Sub btnPopulation_Click(sender As System.Object, e As System.EventArgs) Handles btnPopulation.Click
 
-        MessageBox.Show("Not implemented yet", "Error")
+        Dim pop As Double = 7000000000
+        Dim years As Integer = 2012
+
+        Do While pop > 6000000
+            pop = pop / 2
+            years = years - 50
+        Loop
+
+        MessageBox.Show("The world population was less than 6 million in the year " & years & ".")
 
     End Sub
 End Class
